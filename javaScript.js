ae7364b62a7f482b57c169c17ac00210545e2e0d
adhee <adheenaus@gmail.com>
2019-07-06 02:17:30 -0700

    aaded files

--- javaScript.js
+++ javaScript.js
@@ -16,8 +16,56 @@ document.getElementById("startreset").onclick = function(){
         show("timeremaining");
         timeremaining = 60;
         document.getElementById("timeremainingvalue").innerHTML = timeremaining;
-
-
-
+        hide("gameOver");
+        
+     
+        document.getElementById("startreset").innerHTML = "Reset Game";  
+        
+        startTimer();        
+        generateQAndA();
     }
+}
+
+function startTimer(){
+    calculation = setInterval(function(){
+        timeremaining -= 1;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+        if(timeremaining == 0){// game over
+            stopCountdown();
+            
+            hide("timeremaining");
+            hide("correct");
+            hide("wrong");
+            play = false;
+            document.getElementById("startreset").innerHTML = "Start Game";
+        }
+    }, 1000);    
+}
+
+function generateQAndA(){
+    var x = 1+ Math.round(9*Math.random());
+    var y = 1+ Math.round(9*Math.random());
+    correctAnswer = x*y;
+    document.getElementById("question").innerHTML = x + "x" + y;
+    var correctPos = 1+ Math.round(3*Math.random());
+    document.getElementById("box"+correctPos).innerHTML = correctAnswer;
+    
+
+    
+    var answers = [correctAnswer];
+    
+    for(i=1; i<5; i++){
+        if(i != correctPos) {
+            var wrongAnswer;
+            do{
+                wrongAnswer = (1+ Math.round(9*Math.random()))*(1+ Math.round(9*Math.random())); 
+            }while(answers.indexOf(wrongAnswer)>-1)
+            document.getElementById("box"+i).innerHTML = wrongAnswer;
+            answers.push(wrongAnswer);
+        }
+    }
+}
+
+function stopCountdown(){
+    clearInterval(calculation);   
 }
