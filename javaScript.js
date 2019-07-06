4cf004bdc3af73ab8ab9f8f586ca9af175dc3b3d
adhee <adheenaus@gmail.com>
2019-07-06 01:35:23 -0700

    added timer

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,23 @@
 var play = false;
 var timeremaining;
 var score;
+var calculation;
+var correctAnswer;
+document.getElementById("startreset").onclick = function(){
+    if(play == true)
+    {
+        location.reload();
+    }
+    else
+    {
+        play = true;
+        score = 0;
+        document.getElementById("scorevalue").innerHTML = score;
+        show("timeremaining");
+        timeremaining = 60;
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+
+
+
+    }
+}
